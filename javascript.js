5759a03c05c06123083615ee7960ca76f380e107
SmithRBG <smit36000@gmail.com>
2022-01-09 20:02:00 -0500

    adding event listeners

--- javascript.js
+++ javascript.js
@@ -1,4 +1,52 @@
-//generator functions
+// 3) DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numberEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+// 2) putting functions in object as keys
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+// 4) adding event listeners (When this is clicked we need to get the value)
+//Length of password event listener
+//Generate event Listen
+generateEl.addEventListener('click', () =>{
+    //+ changes string to 'number' same as -parse int-
+    const length = +lengthEl.value;
+    //check to see if boxes are checked (true or false)
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numberEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    //typeof changes actual number to 'number'
+    //console.log(typeof length);
+
+    //confirm if they are true or false when checked.
+    //console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+
+    //function that passes in above + length => result gets passed into result element
+    resultEl.innerText = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbol, 
+        length
+    );
+});
+
+// 5) Genrate Password Function
+
+// 1) generator functions www.net-comber.com/charset.html
 
 function getRandomLower() {
  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -18,7 +66,6 @@ function getRandomSymbol() {
 }
 
 
-console.log(getRandomSymbol());
 
 //returns within the range needed by adding 97
 //console.log(Math.floor(Math.random() * 26) + 97);
